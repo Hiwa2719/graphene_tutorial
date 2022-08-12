76d47d3d8ff2df1a46d2d9769a204081e2e552a8
hiwa <hiwa@DESKTOP-TTKD9F6.localdomain>
2022-08-12 15:28:18 +0430

    adding users query

--- schema.py
+++ schema.py
@@ -1,9 +1,17 @@
 import json
+from datetime import datetime
 
 import graphene
 
 
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
+
+
 class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
     is_admin = graphene.Boolean()
 
@@ -13,15 +21,27 @@ class Query(graphene.ObjectType):
     def resolve_is_admin(self, info):
         return True
 
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username='fred', created_at=datetime.now()),
+            User(id="2", username='john', created_at=datetime.now()),
+        ][:limit]
+
+
 schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     """
     {
-        isAdmin 
+        users {
+         id
+         username
+         createdAt
+         }
     }
     """
 )
 
+print(result)
 result_dict = dict(result.data.items())
 print(json.dumps(result_dict, indent=4))
