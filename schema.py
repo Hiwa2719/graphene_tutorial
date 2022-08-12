f366332313b990bec9fd92d47feac62218050584
hiwa <hiwa@DESKTOP-TTKD9F6.localdomain>
2022-08-12 13:04:36 +0430

    adding is_admin to query fields

--- schema.py
+++ schema.py
@@ -5,17 +5,20 @@ import graphene
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
 
+    def resolve_is_admin(self, info):
+        return True
 
 schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     """
     {
-        hello 
+        isAdmin 
     }
     """
 )
